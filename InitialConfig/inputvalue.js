d191593590ffa3b9806f7538dc89f77a2c3b43c8
Franck <franck.laurent07@orange.fr>
2023-02-21 12:05:15 +0100

    inputvaluebrouillon

---
+++ InitialConfig/inputvalue.js
@@ -0,0 +1,5 @@
+document.getElementById("button1")
+        .addEventListener("click", function() {
+  document.getElementById("container").hidden = true;
+  document.getElementById("containerform").hidden = false;
+}, false);
