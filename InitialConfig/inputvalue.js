95cba056188188b94660ef3cc1c0373cb45cf46a
Franck <franck.laurent07@orange.fr>
2023-02-22 15:00:36 +0100

    inputvaluejs

--- InitialConfig/inputvalue.js
+++ InitialConfig/inputvalue.js
@@ -1,7 +1,21 @@
-var numberValue = value [1, 2, 3, 4];
+// Récupération des éléments HTML 
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const myConfig = document.getElementById("form-container");
 
-function numberValue(value) {
-  for (let index = 0; index < array.lenght; index++) {
-     console.log(value);
+// Ajout des événements click aux boutons
+btn1.addEventListener("click", afficherFormulaire);
+btn2.addEventListener("click", afficherFormulaire);
+btn3.addEventListener("click", afficherFormulaire);
+btn4.addEventListener("click", afficherFormulaire);
+
+// Fonction pour afficher ou masquer le formulaire
+function afficherFormulaire() {
+  if (myConfig.style.display === "none") {
+    myConfig.style.display = "block";
+  } else {
+    myConfig.style.display = "none";
   }
 }
